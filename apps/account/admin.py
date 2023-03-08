c22e0e745c140cdd807e6cf4ad5457db9395ab15
fnabiyevuz <fnabiyevuz@gmail.com>
2023-03-08 16:25:24 +0500

    second

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import User, Author
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ("id", "username", "first_name", "last_name", "phone", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active")
+    list_display_links = ("id", "username", "first_name", "phone")
+    filter_horizontal = ("groups", "user_permissions")
+    readonly_fields = ("date_joined", "last_login")
+    fieldsets = (
+        (_("Personal Info"), {"fields": ("username", "email", "phone", "password")}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser")}),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "phone", "password1", "password2", "is_active", "is_staff",),
+            },
+        ),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "region", "job")
+    list_display_links = ("id", "user",)
+    search_fields = ("user",)
+    date_hierarchy = 'created_at'
+    list_filter = ('position', 'region')
+
+
+admin.site.register(Author, AuthorAdmin)
--- apps/course/models.py
+++ apps/course/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils.translation import gettext as _
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+from ..account.models import Author
+from ..common.models import BaseModel, GenderType, Region
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class Category(BaseModel):
+    name = models.CharField(verbose_name=_('Name'), max_length=100)
+    slug = models.SlugField(verbose_name=_('Slug'), max_length=130)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+
+class Course(BaseModel):
+    category = models.ForeignKey(Category, verbose_name=_('Category'), on_delete=models.CASCADE, related_name='category_courses')
+    # author = models.ForeignKey(Author, verbose_name=_(Author))
