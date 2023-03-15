dbc072b2d56a8e8236290bcdc827c0fa5e680fb3
fnabiyevuz <fnabiyevuz@gmail.com>
2023-03-15 16:26:01 +0500

    chaneg

--- apps/course/urls.py
+++ apps/course/urls.py
@@ -1,3 +1,5 @@
+from unicodedata import name
+
 from django.urls import path
 
 from .views import CategoryList, CategoryRetrieve, CourseList, CourseRetrieve, LessonList, CreateCertificate, \
@@ -5,7 +7,7 @@ from .views import CategoryList, CategoryRetrieve, CourseList, CourseRetrieve, L
 
 urlpatterns = [
     path('course/category/list/', CategoryList.as_view()),
-    path('course/category/<str:slug>', CategoryRetrieve.as_view()),
+    path('course/category/<str:slug>', CategoryRetrieve.as_view(), name='category-retrieve'),
     path('course/list', CourseList.as_view()),
     path('course/<str:slug>', CourseRetrieve.as_view()),
     path('course/lesson/list/<str:slug>', LessonList.as_view()),
